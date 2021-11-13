8419b27f316c53f03d42fd336588918a0f5a2580
Arifin Rahman <arifinrahman6@gmail.com>
2021-11-13 15:09:14 -0500

    Customized server routes and analyzed request data

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 "use strict";
 
+const getJSONString = (obj) => {
+  return JSON.stringify(obj, null, 2);
+};
+
+const routeResponseMap = {
+  "/": "<h1>Welcome to Parrot!</h1>",
+  "/signup": "<h1>Sign Up</h1>",
+  "/login": "<h1>Log In</h1>",
+  "/error": "<h1>Sorry, this page isn't available</h1>",
+};
+
 const port = 3000,
   http = require("http"),
   httpStatus = require("http-status-codes"),
-  app = http.createServer((request, response) => {
-    console.log("Received an incoming request!");
-    response.writeHead(httpStatus.OK, {
+  app = http.createServer((req, res) => {
+    res.writeHead(httpStatus.OK, {
       "Content-Type": "text/html",
     });
 
-    let responseMessage = "<h1>Welcome to Parrot!</h1>";
-    response.write(responseMessage);
-    response.end();
-    console.log("Sent a response : ${responseMessage}");
+    console.log(`Method: ${getJSONString(req.method)}`);
+    console.log(`URL: ${getJSONString(req.url)}`);
+    console.log(`Headers: ${getJSONString(req.headers)}`);
+
+    if (routeResponseMap[req.url]) {
+      res.end(routeResponseMap[req.url]);
+    } else {
+      res.end(routeResponseMap["/error"]);
+    }
   });
 
 app.listen(port);
